b0ff4a2a5bbcbe552ffd78562033bbd8bf2725b1
Dmytro <svcredd@yahoo.com>
2022-06-21 16:35:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const storeName = 'bukake';
+const storeDescription = {
+    budget: 10000,
+    employees:['alex', 'sergey', 'consul'],
+    products:{
+        free: 52,
+        pizza: 30,
+    },
+    open:true
+};
